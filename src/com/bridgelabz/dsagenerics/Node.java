288117ae262b150727f66f15fc15639712691f84
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-08 21:53:18 +0530

    Added:Frequency Of Large Paragraph Phrase Using Hash Table

--- src/com/bridgelabz/dsagenerics/Node.java
+++ src/com/bridgelabz/dsagenerics/Node.java
@@ -1,3 +1,13 @@
+/*@Purpose : Ability to find frequency of words in a large paragraph phrase “Paranoids are not
+ * paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations”
+ * - Use hashcode to find index of the words in the para
+ * - Create LinkedList for each index and store the words and its frequency
+ * - Use LinkedList to do the Hash Table Operation
+ * - To do this create MyMapNode with Key Value Pair and create LinkedList of MyMapNode
+ * @File : Data Structure Using Hash Table
+ * @Author : Kishlay Kishan
+ */
+
 package com.bridgelabz.dsagenerics;
 
 public class Node<K, V> {
