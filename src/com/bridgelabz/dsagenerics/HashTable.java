578501e36b4ca0d19ea0128219a3f263ed558257
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-08 21:19:39 +0530

    Added:WELCOME TO THE DATA STRUCTURE PROBLEM USING JAVA GENERICS

---
+++ src/com/bridgelabz/dsagenerics/HashTable.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.dsagenerics;
+
+public class HashTable {
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO THE DATA STRUCTURE PROBLEM USING JAVA GENERICS");
+
+	}
+
+}
